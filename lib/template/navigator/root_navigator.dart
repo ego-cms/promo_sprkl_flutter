f1658087f77e8c591ded59780ebeac96769db16e
Vlad <vladalogin@gmail.com>
2023-03-14 22:31:31 +0200

    copyrights

--- lib/template/navigator/root_navigator.dart
+++ lib/template/navigator/root_navigator.dart
@@ -1,3 +1,20 @@
+/* ******************************************************** */
+/* 2023 © TheSprkl for Flutter PROMO (www.thesprkl.io)      */
+/* is a proprietary product developed by                    */
+/* EGO Creative Innovations Limited (www.ego-cms.com)       */
+/* This is a reduced demo version of TheSprkl, the rights   */
+/* and terms of use of which are governed by a separate     */
+/* commercial license.                                      */
+/*                                                          */
+/* This promo version is fully functional but contains a    */
+/* limited number of items.                                 */
+/*                                                          */
+/* EULA https://www.thesprkl.io/legal/eula                  */
+/* Support https://www.thesprkl.io/contact-us               */
+/* Discord https://discord.gg/3qjXP9jCra                    */
+/*                                                          */
+/* ******************************************************** */
+
 part of template;
 
 class RootNavigator extends StatefulWidget {
